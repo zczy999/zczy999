228218c0013f21b472ef78ff40e2539156117954
zczy999 <616456019@qq.com>
2022-11-23 19:01:59 +0800

    初始化

---
+++ leetcode/src/main/java/LRU_Cache_146.java
@@ -0,0 +1,100 @@
+import java.util.HashMap;
+
+public class LRU_Cache_146 {
+
+    public HashMap<Integer, Node> LRUMap;
+
+    int capacity, nowCapacity;
+
+
+    public Node sentinel;
+
+    class Node {
+        Node prev, next;
+        int value;
+        int key;
+
+        public Node(int key, int value) {
+            this.value = value;
+            this.key = key;
+        }
+    }
+
+
+    public LRU_Cache_146(int capacity) {
+        nowCapacity = 0;
+        this.capacity = capacity;
+        this.LRUMap = new HashMap<>();
+        this.sentinel = new Node(-1, -1);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+    }
+
+    public int get(int key) {
+        Node node = LRUMap.get(key);
+        if (node == null) {
+            return -1;
+        }
+        if (node.prev != sentinel) {
+            // delete
+            delete(node);
+            // add
+            addNew(node);
+        }
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        Node node = LRUMap.get(key);
+
+        if (node != null) {
+            delete(node);
+            nowCapacity--;
+        }
+        if (capacity == nowCapacity) {
+
+            LRUMap.remove(sentinel.prev.key);
+            delete(sentinel.prev);
+
+            nowCapacity--;
+        }
+
+        node = new Node(key, value);
+        LRUMap.put(key, node);
+        addNew(node);
+
+
+        nowCapacity++;
+
+    }
+
+    private void delete(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void addNew(Node node) {
+        node.next = sentinel.next;
+        sentinel.next = node;
+        node.prev = sentinel;
+        node.next.prev = node;
+    }
+
+    public static void main(String[] args) {
+        LRU_Cache_146 lRUCache = new LRU_Cache_146(2);
+        lRUCache.put(1, 1); // 缓存是 {1=1}
+        lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+        System.out.println(lRUCache.get(1));    // 返回 1
+        lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+        System.out.println(lRUCache.get(2));
+        //lRUCache.get(2);    // 返回 -1 (未找到)
+        lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+        System.out.println(lRUCache.get(1));
+        //lRUCache.get(1);    // 返回 -1 (未找到)
+        System.out.println(lRUCache.get(3));
+        //lRUCache.get(3);    // 返回 3
+        System.out.println(lRUCache.get(4));
+        //lRUCache.get(4);    // 返回 4
+    }
+}
---
+++ leetcode/src/main/java/Two_Sum_1.java
@@ -0,0 +1,61 @@
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.concurrent.locks.Lock;
+
+public class Two_Sum_1 {
+    public int[] twoSum(int[] nums, int target) {
+        int[][] dnums = new int[nums.length][2];
+        for (int i = 0; i < nums.length; i++) {
+            dnums[i] = new int[]{nums[i], i};
+        }
+
+        Arrays.sort(dnums, (o1, o2) -> o1[0] - o2[0]);
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            if (dnums[left][0] + dnums[right][0] > target) {
+                right--;
+            } else if (dnums[left][0] + dnums[right][0] < target) {
+                left++;
+            } else {
+                return new int[]{dnums[left][1], dnums[right][1]};
+            }
+
+
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static Date covertTime(Date date)  {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Timestamp now = new Timestamp(date.getTime());
+        String str = df.format(now);
+        Date newDate = null;
+        try {
+            newDate = df.parse(str);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        return newDate;
+    }
+
+    public static void main(String[] args) {
+        Lock
+//        Date date = new Date();
+        String tsStr = "2011-05-09 11:49:45.999";
+        Date  date = Timestamp.valueOf(tsStr);
+        System.out.println(date);
+//        date = covertTime(date);
+        long time = date.getTime();
+        System.out.println(time);
+        time = time/1000*1000;
+        System.out.println(time);
+        date = new Date(time);
+        System.out.println(date);
+    }
+}
