60b7f0909cc82b11059d7db145a15bb22dd602c8
zczy999 <616456019@qq.com>
2023-04-02 14:38:28 +0800

    完成redis一些测试

--- spring-redis/src/test/java/org/tsymq/myredis/MyRedisTest.java
+++ spring-redis/src/test/java/org/tsymq/myredis/MyRedisTest.java
@@ -1,15 +1,20 @@
 package org.tsymq.myredis;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 public class MyRedisTest {
 
     @Autowired
     private RedisTemplate redisTemplate;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Test
     public void testStringSet() throws Exception {
@@ -18,7 +23,13 @@ public class MyRedisTest {
 
     @Test
     public void testStringGet() throws Exception {
-        System.out.println(redisTemplate.opsForValue().get("zc"));
+        Object zc = redisTemplate.opsForValue().get("zc");
+        //使用了jackson序列化配置后获取string会多双引号
+        String zc1 = stringRedisTemplate.opsForValue().get("zc");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String valueWithoutQuotes = objectMapper.readValue(zc1, String.class);
+        System.out.println((String)redisTemplate.opsForValue().get("zc"));
+        System.out.println(stringRedisTemplate.opsForValue().get("zc"));
     }
 
 }
