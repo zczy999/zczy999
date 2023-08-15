78a729bf3e25d0a18f04d389083a764ff6931777
zczy999 <616456019@qq.com>
2023-08-15 11:32:40 +0800

    完善leetcode第274题

--- leetcode/src/main/java/leetcode/HIndex_274.java
+++ leetcode/src/main/java/leetcode/HIndex_274.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 
 public class HIndex_274 {
     public static int hIndex(int[] citations) {
+        if (citations.length == 1 && citations[0] == 0) {
+            return 0;
+        }
         Arrays.sort(citations);
         int len = citations.length;
         int left = 0;
@@ -11,10 +14,13 @@ public class HIndex_274 {
         while (left + 1 < right) {
             int mid = (left + right) / 2;
             if (citations[mid] >= len - mid) {
-                right = mid - 1;
+                right = mid;
             } else {
-                left = mid + 1;
+                left = mid;
             }
+        }
+        if (citations[left] == 0 && citations[right] == 0) {
+            return 0;
         }
         if (citations[left] >= len - left) {
             return len - left;
@@ -23,8 +29,9 @@ public class HIndex_274 {
     }
 
     public static void main(String[] args) {
-        int citations = {3,0,6,1,5};
-        HIndex_274.hIndex(citations).var
+        int[] citations = {3, 0, 6, 1, 5};
+        int i = HIndex_274.hIndex(citations);
+        System.out.println(i);
     }
 
 
