a23fa893a678bd80f99ed2d5f2c8e2ddb708fe99
唐宋元明清 <616456019@qq.com>
2023-10-12 11:31:40 +0800

    完成leetcode 第3题

---
+++ leetcode/src/main/java/leetcode/Longest_Substring_Without_Repeating_Characters_3.java
@@ -0,0 +1,53 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Longest_Substring_Without_Repeating_Characters_3 {
+    public static void main(String[] args) {
+        Longest_Substring_Without_Repeating_Characters_3 res = new Longest_Substring_Without_Repeating_Characters_3();
+        int i = res.lengthOfLongestSubstring("abba");
+        System.out.println(i);
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        int index = 0;
+        int left = 0;
+        int max = 0;
+        while (index < s.length()) {
+            if (map.containsKey(s.charAt(index))) {
+                //这个max很重要，排除不在这个滑动窗口里的值，这种情况下不需要更新
+                left = Math.max(left,map.get(s.charAt(index)) + 1);
+            }
+
+            map.put(s.charAt(index), index);
+            max = Math.max(max, index - left + 1);
+            index++;
+        }
+        return max;
+    }
+
+    public int lengthOfLongestSubstring1(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        int index = 0;
+        int res = 0;
+        int max = 0;
+        while (index < s.length()) {
+            if (map.containsKey(s.charAt(index))) {
+                res = 0;
+                index = map.get(s.charAt(index)) + 1;
+                map.clear();
+            } else {
+                map.put(s.charAt(index), index);
+                res++;
+                max = Math.max(max, res);
+                index++;
+            }
+        }
+        return max;
+    }
+
+}
