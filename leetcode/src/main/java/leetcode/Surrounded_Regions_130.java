d9d59d377de42c1863f919c530c92827bd68f17f
唐宋元明清 <616456019@qq.com>
2023-11-06 21:20:06 +0800

    完善leetcode 第130题

--- leetcode/src/main/java/leetcode/Surrounded_Regions_130.java
+++ leetcode/src/main/java/leetcode/Surrounded_Regions_130.java
@@ -10,21 +10,14 @@ public class Surrounded_Regions_130 {
         int[][] flag = new int[n][m];
 
         for (int i = 0; i < n; i++) {
-            if (board[i][0] == 'O') {
-                dfs(i, 0, flag, board);
-            }
-            if (board[i][m - 1] == 'O') {
-                dfs(i, m - 1, flag, board);
-            }
+            dfs(i, 0, flag, board);
+            dfs(i, m - 1, flag, board);
         }
 
+
         for (int j = 0; j < m; j++) {
-            if (board[0][j] == 'O') {
-                dfs(0, j, flag, board);
-            }
-            if (board[n - 1][j] == 'O') {
-                dfs(n - 1, j, flag, board);
-            }
+            dfs(0, j, flag, board);
+            dfs(n - 1, j, flag, board);
         }
 
         for (int i = 0; i < n; i++) {
