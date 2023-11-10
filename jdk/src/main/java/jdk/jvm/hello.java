f29c09b7aa21abeb8ec465129d38ec1a381d749e
zczy999 <616456019@qq.com>
2023-11-10 11:52:07 +0800

    jvm字节码分析

---
+++ jdk/src/main/java/jdk/jvm/hello.java
@@ -0,0 +1,15 @@
+package jdk.jvm;
+
+/**
+ * java字节码解读
+ * javap -verbose hello
+ */
+public class hello {
+    private final static int num = 1000;
+    public static  String zc = "tsymq";
+    public int add() {
+//        num = num + 2;
+//        return num;
+        return 0;
+    }
+}
