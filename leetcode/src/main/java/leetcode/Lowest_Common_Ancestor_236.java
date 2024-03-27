1fb89bf971b8efba42dfd0455f2940998b82a547
zczy999 <616456019@qq.com>
2024-03-27 10:58:27 +0800

    完成leetcode第236题LCA

---
+++ leetcode/src/main/java/leetcode/Lowest_Common_Ancestor_236.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+public class Lowest_Common_Ancestor_236 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p) {
+            return p;
+        }
+        if (root == q) {
+            return q;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left != null) {
+            return left;
+        }
+        if (right != null) {
+            return right;
+        }
+
+
+        return null;
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
