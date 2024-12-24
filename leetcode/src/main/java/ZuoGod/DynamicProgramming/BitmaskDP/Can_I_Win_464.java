ecd551edd1b8d655db252cabcb2f78e2b35e1e22
zczy999 <616456019@qq.com>
2024-12-24 11:40:17 +0800

    feat(DynamicProgramming): 添加 Can_I_Win_464.java 文件
    - 实现了 Can I Win? 问题的解决方案
    - 使用位掩码动态规划算法
    - 处理了多种情况，包括 desiredTotal 为 0 和超过最大可选整数总和的情况

---
+++ leetcode/src/main/java/ZuoGod/DynamicProgramming/BitmaskDP/Can_I_Win_464.java
@@ -0,0 +1,38 @@
+package ZuoGod.DynamicProgramming.BitmaskDP;
+
+public class Can_I_Win_464 {
+
+    public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+        if (desiredTotal == 0) {
+            return true;
+        }
+        if (desiredTotal > maxChoosableInteger * (maxChoosableInteger + 1) / 2) {
+            return false;
+        }
+        int[] dp = new int[1 << (maxChoosableInteger + 1)];
+        return f(maxChoosableInteger, desiredTotal, (1 << maxChoosableInteger + 1) - 1, 0, dp);
+
+    }
+
+    // 返回当前的先手能不能赢，能赢返回true，不能赢返回false
+    private boolean f(int maxChoosableInteger, int desiredTotal, int state, int currSum, int[] dp) {
+        //当过来到的数字和已经使用的数字的和大于desiredTotal，说明对手已经赢了，返回false
+        if (currSum >= desiredTotal) {
+            return false;
+        }
+        if (dp[state] != 0) {
+            return dp[state] == 1;
+        }
+
+        boolean ans = false;
+        for (int i = 1; i <= maxChoosableInteger; i++) {
+            //如果i没用 且使用后对手没赢
+            if ((state & (1 << i)) != 0 && !f(maxChoosableInteger, desiredTotal, state ^ (1 << i), currSum + i, dp)) {
+                ans = true;
+                break;
+            }
+        }
+        dp[state] = ans ? 1 : -1;
+        return ans;
+    }
+}
