adb538eb3929906a0256bda4e905656f6fa816f3
zczy999 <616456019@qq.com>
2025-03-01 12:11:41 +0800

    feat(DynamicProgramming): 添加新题解 kIncreasing 数组的最小操作数
    - 新增 Minimum_Operations_to_Make_the_Array_K_Increasing_2111 类
    - 实现 kIncreasing 方法来计算数组的最小操作数
    - 添加 main 方法进行测试验证

---
+++ leetcode/src/main/java/ZuoGod/DynamicProgramming/Minimum_Operations_to_Make_the_Array_K_Increasing_2111.java
@@ -0,0 +1,52 @@
+package ZuoGod.DynamicProgramming;
+
+public class Minimum_Operations_to_Make_the_Array_K_Increasing_2111 {
+
+    public static void main(String[] args) {
+        Minimum_Operations_to_Make_the_Array_K_Increasing_2111 res = new Minimum_Operations_to_Make_the_Array_K_Increasing_2111();
+        int[] nums = {4, 1, 5, 2, 6, 2};
+        System.out.println(res.kIncreasing(nums, 2));
+    }
+
+    public int kIncreasing(int[] nums, int k) {
+
+        int time = 0;
+        int res = 0;
+        while (time < k) {
+            int[] end = new int[(nums.length + k - 1) / k];
+            end[0] = nums[time];
+            int increasingLen = 1;
+            int len = 1;
+            for (int i = time + k; i < nums.length; i = i + k) {
+                len++;
+                int rightPos = findRightPos(end, increasingLen, nums[i]);
+                if (rightPos == -1) {
+                    end[increasingLen++] = nums[i];
+                } else {
+                    end[rightPos] = nums[i];
+                }
+
+            }
+            res += len - increasingLen;
+            time++;
+        }
+
+        return res;
+    }
+
+    private int findRightPos(int[] end, int len, int target) {
+        int ans = -1;
+        int left = 0, right = len;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (end[mid] > target) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+}
