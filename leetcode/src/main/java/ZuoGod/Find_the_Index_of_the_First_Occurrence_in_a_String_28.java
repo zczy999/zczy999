83c4b5d68a46880a9b444c9f890a4987d10e3854
zczy999 <616456019@qq.com>
2025-03-15 11:42:30 +0800

    feat(ZuoGod): 实现字符串哈希和 KMP 算法
    - 添加了字符串哈希方法 strStr 实现字符串匹配
    - 移除了未使用的 HashMap 导入
    - 添加了测试用例

--- leetcode/src/main/java/ZuoGod/Find_the_Index_of_the_First_Occurrence_in_a_String_28.java
+++ leetcode/src/main/java/ZuoGod/Find_the_Index_of_the_First_Occurrence_in_a_String_28.java
@@ -1,7 +1,7 @@
 package ZuoGod;
 
 import java.util.Arrays;
-import java.util.HashMap;
+
 
 public class Find_the_Index_of_the_First_Occurrence_in_a_String_28 {
 
@@ -10,9 +10,73 @@ public class Find_the_Index_of_the_First_Occurrence_in_a_String_28 {
         Find_the_Index_of_the_First_Occurrence_in_a_String_28 res = new Find_the_Index_of_the_First_Occurrence_in_a_String_28();
         int[] next = res.findNext(s.toCharArray());
         System.out.println(Arrays.toString(next));
+        String haystack = "hello";
+        String needle = "ll";
+        int i = res.strStr(haystack, needle);
+        System.out.println(i);
     }
 
+    int base = 131;
+    long[] hashArray;
+    long[] pow;
+
+    /**
+     * 字符串哈希
+     * @param haystack
+     * @param needle
+     * @return
+     */
     public int strStr(String haystack, String needle) {
+
+        build(haystack);
+
+        long needleHash = hash(needle);
+        for (int i = 0, j = needle.length() - 1; j < haystack.length(); i++, j++) {
+            if (hash(hashArray,pow, i, j) == needleHash){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void build(String haystack){
+        hashArray = new long[haystack.length()];
+        hashArray[0] = haystack.charAt(0) - 'a' + 1;
+        for (int i = 1; i < haystack.length(); i++) {
+            hashArray[i] = hashArray[i - 1] * base + haystack.charAt(i) - 'a' + 1;
+        }
+
+        pow = new long[haystack.length()];
+        pow[0] = 1;
+        for (int k = 1; k < haystack.length(); k++) {
+            pow[k] = pow[k-1] * base;
+        }
+    }
+
+    private long hash(long[] hashArray,long[] pow, int i, int j) {
+        long hash = hashArray[j];
+        if (i > 0) {
+            hash = hash - hashArray[i - 1] *pow[j - i + 1];
+        }
+        return hash;
+    }
+
+    private long hash(String s) {
+        long hash = 0;
+        for (int i = 0; i < s.length(); i++) {
+            hash = hash * base + s.charAt(i) - 'a' + 1;
+        }
+        return hash;
+    }
+
+    /**
+     * kmp
+     *
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    public int strStr1(String haystack, String needle) {
 
         int n = haystack.length(), m = needle.length();
         char[] haystackChars = haystack.toCharArray();
