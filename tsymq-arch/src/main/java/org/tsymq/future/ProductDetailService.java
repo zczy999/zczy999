69da54f75ba1f942eed46006bfe87457d37ec005
zczy999 <616456019@qq.com>
2025-03-19 11:15:20 +0800

    feat(tsymq-arch): 添加 Future 相关测试代码
    - 新增 EduService 类，用于模拟不同服务调用
    - 添加 FutureTest 类，包含多个测试方法演示 Future 的使用
    - 新增 Product、ProductDetailService 和 ProductService 类，用于测试任务编排和异步处理
    - 在 pom.xml 中添加 junit 依赖，用于单元测试

---
+++ tsymq-arch/src/main/java/org/tsymq/future/ProductDetailService.java
@@ -0,0 +1,35 @@
+package org.tsymq.future;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 小滴课堂,愿景：让技术不再难学
+ *
+ * @Description
+ * @Author 二当家小D
+ * @Remark 有问题直接联系我，源码-笔记-技术交流群
+ * @Version 1.0
+ **/
+public class ProductDetailService {
+    private static final Map<Integer,String> map = new HashMap<>();
+    static {
+        map.put(1,"架构大课-详情图内容");
+        map.put(2,"海量数据项目大课-详情图内容");
+        map.put(3,"高并发架构大课-详情图内容");
+        map.put(4,"在线教育平台-详情图内容");
+        map.put(5,"小滴课堂永久会员-详情图内容");
+        map.put(6,"java入门到放弃-详情图内容");
+        map.put(7,"颈椎病康复指南-详情图内容");
+    }
+
+    public String getById(int id){
+        try {
+            Thread.sleep(3000);
+            System.out.println("DetailService # getById方法运行线程："+Thread.currentThread().getName());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return map.get(id);
+    }
+}
