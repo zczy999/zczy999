52b7db36c46ba5e2106f3a21436d026af0a9e0bc
zczy999 <616456019@qq.com>
2025-04-09 11:27:55 +0800

    feat(Binary_Tree_Right_Side_View_199): 添加二叉树右视图的 DFS 解法
    - 新增 dfs 方法实现二叉树的右视图遍历
    - 使用递归深度优先搜索算法
    - 优化了原有的 bfs 方法，提高了代码的可读性和效率

--- leetcode/src/main/java/leetcode/Binary_Tree_Right_Side_View_199.java
+++ leetcode/src/main/java/leetcode/Binary_Tree_Right_Side_View_199.java
@@ -3,7 +3,36 @@ package leetcode;
 import java.util.*;
 
 public class Binary_Tree_Right_Side_View_199 {
+
+    Set<Integer> set;
+    List<Integer> res;
     public List<Integer> rightSideView(TreeNode root) {
+        this.set = new HashSet<>();
+        this.res = new ArrayList<>();
+        dfs(root,0);
+        return res;
+    }
+
+    public void dfs(TreeNode root,int depth) {
+        if (root == null){
+            return;
+        }
+//        if (!set.contains(depth)){
+        if (res.size() <= depth){
+            res.add(root.val);
+            set.add(depth);
+        }
+        dfs(root.right,depth+1);
+        dfs(root.left,depth+1);
+    }
+
+
+    /**
+     * bfs
+     * @param root
+     * @return
+     */
+    public List<Integer> rightSideView1(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         Deque<TreeNode> deque = new ArrayDeque<>();
         if (root == null) {
